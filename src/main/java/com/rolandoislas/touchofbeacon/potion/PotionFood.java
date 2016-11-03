d9f4754618b1d6322b3d4c27569f690d09dd46e3
Rolando Islas <admin@rolandoislas.com>
2016-11-02 18:09:26 -0700

    Add icon to potion effect

--- src/main/java/com/rolandoislas/touchofbeacon/potion/PotionFood.java
+++ src/main/java/com/rolandoislas/touchofbeacon/potion/PotionFood.java
@@ -2,14 +2,20 @@ package com.rolandoislas.touchofbeacon.potion;
 
 import com.rolandoislas.touchofbeacon.TouchOfBacon;
 import com.rolandoislas.touchofbeacon.blocks.EnumTier;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * Created by Rolando on 10/31/2016.
  */
 public class PotionFood extends Potion {
+	ResourceLocation icon = new ResourceLocation(TouchOfBacon.MODID, "textures/icons/potion_fed.png");
+
 	public PotionFood() {
 		super(false, 1);
 		this.setRegistryName("fed");
@@ -32,5 +38,17 @@ public class PotionFood extends Potion {
 	@Override
 	public boolean isReady(int duration, int amplifier) {
 		return true;
+	}
+
+	@Override
+	public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+		mc.renderEngine.bindTexture(icon);
+		Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 16, 16, 16, 16);
+	}
+
+	@Override
+	public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+		mc.renderEngine.bindTexture(icon);
+		Gui.drawModalRectWithCustomSizedTexture(x + 4, y + 4, 0, 0, 16, 16, 16, 16);
 	}
 }
