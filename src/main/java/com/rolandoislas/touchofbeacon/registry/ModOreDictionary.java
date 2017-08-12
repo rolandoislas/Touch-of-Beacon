b88cb3b9ed6f9edbb98326f0403d59a5a34cce3a
Rolando Islas <admin@rolandoislas.com>
2017-08-12 13:29:56 -0700

    Fix server 1.11 crash

--- src/main/java/com/rolandoislas/touchofbeacon/registry/ModOreDictionary.java
+++ src/main/java/com/rolandoislas/touchofbeacon/registry/ModOreDictionary.java
@@ -1,13 +1,7 @@
 package com.rolandoislas.touchofbeacon.registry;
 
-import com.rolandoislas.touchofbeacon.blocks.EnumFood;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.NonNullList;
 import net.minecraftforge.oredict.OreDictionary;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Rolando on 11/1/2016.
@@ -19,20 +13,6 @@ public class ModOreDictionary {
 		// Register sea lantern with ore dict since it does not have an entry
 		OreDictionary.registerOre("sealantern", Blocks.SEA_LANTERN);
 		// Food block
-		NonNullList<ItemStack> foodBlockItems = NonNullList.create();
-		ModItems.FOOD.getSubItems(ModItems.FOOD, ModItems.FOOD.getCreativeTab(), foodBlockItems);
-		for (ItemStack item : foodBlockItems) {
-			OreDictionary.registerOre(FOOD_BLOCK, item);
-			OreDictionary.registerOre(FOOD_BLOCK + getSubItemOreDictSuffix(item), item);
-		}
-	}
-
-	private static String getSubItemOreDictSuffix(ItemStack item) {
-		String suffix = "";
-		String name = EnumFood.getFoodFromMeta(item.getMetadata()).getUnlocalizedName();
-		String[] nameParts = name.split("_");
-		for (String part : nameParts)
-			suffix += part.substring(0, 1).toUpperCase() + part.substring(1);
-		return suffix;
+		OreDictionary.registerOre(FOOD_BLOCK, ModBlocks.FOOD);
 	}
 }
